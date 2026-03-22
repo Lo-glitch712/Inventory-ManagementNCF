3ce01b4425da94a0ffc94216614e389e0fa61f29
LonMonserate <lamonserate703.pbox@parsu.edu.ph>
2026-03-23 00:48:58 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API = "https://script.google.com/macros/s/AKfycbxtrYXn90XeWU1w6XRblInKLKDQowjzttq7cQmBzmM9ouzswziqBEm9XOrfpsvZpy-s/exec";
+const API = "https://script.google.com/macros/s/AKfycbws_TGd-Xe8lQuWRgjryBxNtyuTyvDo8BSajOVZgRspAjmzgCK-bLfzghtVVkRAoS10/exec";
 
 
 // Helper function for showing notifications
